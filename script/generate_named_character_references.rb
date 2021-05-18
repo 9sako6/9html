1b57e874dce944660172c6cb34b5a68337e74e0a
9sako6 <31821663+9sako6@users.noreply.github.com>
2021-05-18 21:28:23 +0900

    Add named characters
    13.5 Named character references
    https://html.spec.whatwg.org/multipage/named-characters.html#named-character-references
    https://html.spec.whatwg.org/entities.json

---
+++ script/generate_named_character_references.rb
@@ -0,0 +1,34 @@
+#
+# Usage:
+#   `ruby script/generate_named_character_references.rb` in the root directory.
+# https://html.spec.whatwg.org/entities.json
+
+require 'open-uri'
+require 'json'
+
+OUTPUT_PATH = './lib/htimlee/tokenizer/named_character.rb'.freeze
+
+entities_json = JSON.parse(URI.open('https://html.spec.whatwg.org/entities.json', &:read))
+
+source_code = <<~SOURCECODE
+  # rubocop:disable all
+  # This code is generated automatically with #{__FILE__}.
+  module Htimlee
+    class Tokenizer
+      module NamedCharacter
+        CODEPOINTS = {
+  #{
+    entities_json.map do |character_name, character_entity|
+      # codepoints = character_entity['codepoints'].map
+      # entity = character_entity['codepoints'].pack('U')
+      "        '#{character_name}'.freeze => #{character_entity['codepoints']}.freeze,"
+    end.join("\n")
+  }
+        }.freeze
+      end
+    end
+  end
+  # rubocop:enable all
+SOURCECODE
+
+File.open(OUTPUT_PATH, 'w') { |f| f.puts source_code }
